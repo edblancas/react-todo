e953fdf0c8379c1a69e1a8adae9507c165e239d2
Daniel Blancas <edblancas@gmail.com>
2018-06-12 13:45:42 -0500

    Add TOGGLE_TODO action.

--- src/example-tests/todo-reducer.test.js
+++ src/example-tests/todo-reducer.test.js
@@ -11,6 +11,17 @@ const todos = (state = [], action) => {
           completed: action.completed,
         },
       ]
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        if (todo.id === action.id) {
+         return {
+           ...todo,
+           completed: !todo.completed
+         }
+        }
+
+        return todo;
+      })
     default:
       return state
   }
@@ -34,3 +45,39 @@ test('test add todo action', () => {
     todos(stateBefore, action),
   ).toEqual(stateAfter)
 })
+
+test('toggle todo action reducer', () => {
+  const stateBefore = [
+    {
+      id: 0,
+      text: 'Learning Redux!',
+      completed: false,
+    },
+    {
+      id: 1,
+      text: 'Learning React!',
+      completed: false,
+    },
+  ]
+  const action = {type: 'TOGGLE_TODO', id: 0}
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learning Redux!',
+      completed: true,
+    },
+    {
+      id: 1,
+      text: 'Learning React!',
+      completed: false,
+    },
+  ]
+
+  deepFreeze(stateBefore)
+  deepFreeze(action)
+
+  expect(
+    todos(stateBefore, action),
+  ).toEqual(stateAfter)
+})
+
