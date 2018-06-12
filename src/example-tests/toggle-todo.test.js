ef7bb5bc781216db02128714a35aa964e9189e49
Daniel Blancas <edblancas@gmail.com>
2018-06-12 13:25:02 -0500

    Add toggle todo test.

---
+++ src/example-tests/toggle-todo.test.js
@@ -0,0 +1,49 @@
+import deepFreeze from 'deep-freeze'
+
+const toggleTodoMutated = todo => {
+  todo.completed = !todo.completed
+  return todo
+}
+
+test('test mutated object', () => {
+  const todoBefore = {
+    id: 0,
+    text: 'Learn Redux!',
+    completed: true
+  }
+
+  const todoAfter = {
+    id: 0,
+    text: 'Learn Redux!',
+    completed: false
+  }
+
+  deepFreeze(todoBefore)
+
+  expect(toggleTodoMutated(todoBefore)).toEqual(todoAfter)
+})
+
+const toggleTodo = todo => {
+  return {
+    ...todo,
+    completed: !todo.completed
+  }
+}
+
+test('test new todo object return', () => {
+   const todoBefore = {
+    id: 0,
+    text: 'Learn Redux!',
+    completed: true
+  }
+
+  const todoAfter = {
+    id: 0,
+    text: 'Learn Redux!',
+    completed: false
+  }
+
+  deepFreeze(todoBefore)
+
+  expect(toggleTodo(todoBefore)).toEqual(todoAfter)
+})
