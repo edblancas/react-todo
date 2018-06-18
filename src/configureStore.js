590781c57aeffb47b4705ddf0cba1e514ebccb00
Daniel Blancas <edblancas@gmail.com>
2018-06-18 10:54:21 -0500

    12. Wrapping dispatch() to Log Actions

--- src/configureStore.js
+++ src/configureStore.js
@@ -6,6 +6,8 @@ import todoApp from './reducers'
 const configureStore = () => {
   const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   const store = createStore(todoApp, loadState(), reduxDevTools)
+  if (process.env.NODE_ENV !== 'production')
+    store.dispatch = addLoggingToDispatch(store)
 
   store.subscribe(
     throttle(() => {
@@ -19,3 +21,22 @@ const configureStore = () => {
 }
 
 export default configureStore
+
+const addLoggingToDispatch = (store) => {
+  // If we dont obtain the raw dispatch, when we change the store.dispatch we
+  // are going to have a recursive ref, cause the store.dispatch inside the
+  // returning function is going to ref the dispatch changed ref
+  const rawDispatch = store.dispatch
+  return (action) => {
+    if (!console.group) return rawDispatch
+
+    console.group(action.type)
+    console.log('%c prev state', 'color: gray', store.getState())
+    console.log('%c action', 'color: blue', action)
+    const returnValue = rawDispatch(action)
+    console.log('%c next state', 'color: green', store.getState())
+    console.groupEnd(action.type)
+
+    return returnValue
+  }
+}
