b90798cecaf323617e73391e671b36ea134d5c51
Daniel Blancas <edblancas@gmail.com>
2018-06-19 12:29:23 -0500

    18.1 Applying Redux Middleware (using applyMiddleware from react)

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,17 +1,14 @@
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import todoApp from './reducers'
 
 const configureStore = () => {
   const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  const store = createStore(todoApp, reduxDevTools)
   const middlewares = [promise]
 
   if (process.env.NODE_ENV !== 'production')
     middlewares.push(logger)
 
-  applyMiddlewareToDispatch(store, middlewares)
-
-  return store
+  return createStore(todoApp, applyMiddleware(...middlewares))
 }
 
 export default configureStore
@@ -45,10 +42,4 @@ const logger = store => next => action => {
 const promise = store => next => action => {
   if (typeof action.then === 'function') return action.then(next)
   return next(action)
-}
-
-const applyMiddlewareToDispatch = (store, middlewares) => {
-  middlewares.slice().reverse().forEach(middleware => {
-    store.dispatch = middleware(store)(store.dispatch)
-  })
 }
