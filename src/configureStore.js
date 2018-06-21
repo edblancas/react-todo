30b30ee1ac38157515d8b33affcc7771c84a3ba3
Daniel Blancas <edblancas@gmail.com>
2018-06-21 15:10:57 -0500

    Add redux-thunk npm package

--- src/configureStore.js
+++ src/configureStore.js
@@ -2,14 +2,7 @@ import {createStore, applyMiddleware, compose} from 'redux'
 import todoApp from './reducers'
 import {createLogger} from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
-
-// Note: remove the brackets so it returns the pomise, if not it returns undefined
-const thunk = store => next => action =>
-  typeof action === 'function'?
-    // it must be the store.dispatch, cause the next dispatch middleware
-    // in chain
-    action(store.dispatch, store.getState) :
-    next(action)
+import thunk from 'redux-thunk'
 
 export const sagaMiddleware = createSagaMiddleware()
 
