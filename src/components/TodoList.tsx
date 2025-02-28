4aaa745e2b83cdda36e14e68a4f2d87eb50df9e1
Daniel Blancas <edblancas@gmail.com>
2025-02-28 11:08:21 -0600

    add explanation for short circuiting, conditional rendering with &&

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,6 +3,7 @@ import { useTodos } from "../App";
 
 export default function TodoList() {
   const { todos, setTodos } = useTodos()
+  // The <ol> element is a separate JSX element that is rendered regardless of the outcome of the short-circuited expression. This means that even if "no todos" is displayed when there are no todos, the <ol> element is still rendered – it just happens to be empty because todos.map(...) produces no items.
   return (
     <>
       {todos.length === 0 && "no todos"}
